724f32181ac23ef41f5085d663c60615cda390d8
MP_Praewrung <38246225+meipraewrung@users.noreply.github.com>
2020-10-04 05:58:21 +0700

    connect database

---
+++ db.php
@@ -0,0 +1,12 @@
+<?php
+    $connect = mysqli_connect('localhost','root','','LoadUnitPlan');
+                mysqli_query($connect,"set character set utf8");
+ 
+    // if(!$connect) {
+    //     die(' Please Check Your Connection'.mysqli_error($connect));
+    // }
+
+    if(mysqli_connect_error()) {
+        echo "Failed to connect to MySQL :".mysqli_connect_error();
+    }
+?>
