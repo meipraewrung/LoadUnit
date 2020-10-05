433a18720f1cb2264a779e6bd83677ff71cf1cac
JulalakTammaphet <48485581+JulalakTammaphet@users.noreply.github.com>
2020-10-05 21:22:55 +0700

    Update getdata.php

--- php/getdata.php
+++ php/getdata.php
@@ -1,30 +1,24 @@
-<?php 
-//หน้าเล่มหลักสูตร
-$conn = mysqli_connect("localhost","root", "", "loadunitplan");
-$sql = "SELECT * FROM plancourse";
-$result = mysqli_query($conn, $sql);
 
 
-while ($row = mysqli_fetch_assoc($result))
-{
-    $data[] = $row;
-}
+<?php
+ // Connects to your Database
+ //ดึงข้อมูล
+ mysql_connect("localhost", "root", "6030213020") or die(mysql_error());
+ mysql_select_db("LoadUnitPlan") or die(mysql_error());
+ $data = mysql_query("SELECT * FROM opencourse")
+ or die(mysql_error());
+ print "<table border cellpadding=3>";
+ while($info = mysql_fetch_array( $data ))
+ {
+    
+    print "<b>หมวด</b> ".$info['openCourse_category'] . " ";
+    print "<b>กลุ่ม</b> ".$info['openCourse_group'] . " ";
+    print "<b>บังคับ/เลือก</b> ".$info['openCourse_forceSelect'] . " ";
+    print "<b>รหัสวิชา</b> ".$info['openCourse_courseCode'] . " ";
+    print "<b>รายชื่อวิชา</b> ".$info['openCourse_listSubjects'] . " ";
+    print "<b>หน่วยกิต</b> ".$info['openCourse_credit'] . " ";
+    print "<b>ผู้สอน</b> ".$info['openCourse_instructo'] . " <br>";
+ }
+ print "</table>";
+ ?>
 
-echo json_encode($data);
-
-//หน้าวางเเผนเปิดรายวิชา
-$conn = mysqli_connect("localhost","root", "", "loadunitplan");
-$sql = "SELECT * FROM opencourse";
-$result = mysqli_query($conn, $sql);
-
-
-while ($row = mysqli_fetch_assoc($result))
-{
-    $data[] = $row;
-}
-
-echo json_encode($data);
-
-
-
-?>
